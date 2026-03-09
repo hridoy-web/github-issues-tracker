ce057415930925457426f8fc0c99f1918aa56b2c
Hridoy Chowdhury <t.okay383@gmail.com>
2026-03-09 23:09:42 +0600

    card level fixed

--- script/dashboard.js
+++ script/dashboard.js
@@ -10,8 +10,27 @@ const closedBtn = document.getElementById('btn-closed')
 // 1-2: get the search input
 const searchInput = document.getElementById('search-input')
 
+// loading spinner function
+const loadingSpinner = (loading) => {
+
+    // get the container
+    const spinnerContainer = document.getElementById('loading-spinner');
+
+    // condition apply
+    if (loading) {
+        spinnerContainer.classList.remove('hidden')
+        issueContainer.classList.add('hidden')
+    } else {
+        spinnerContainer.classList.add('hidden');
+        issueContainer.classList.remove('hidden');
+    }
+}
+
 // 02- All issue Api data fatch function 
 const loadAllIssues = (status = 'all') => {
+
+    loadingSpinner(true);
+
     const apiUrl = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
     fetch(apiUrl)
         .then(res => res.json())
@@ -28,6 +47,8 @@ const loadAllIssues = (status = 'all') => {
             }
 
             tabStyle(status);
+
+            loadingSpinner(false);
         })
 }
 
@@ -52,22 +73,29 @@ const displayIssues = (issues) => {
         // 3-6: parent div card design
         newCardDiv.className = `card bg-white p-4 shadow-md rounded-lg border-t-4 ${cardBorder} h-full flex flex-col justify-between`
 
+        // 3-7: daisyUi level badge
+        const labelsBox = issue.labels.map(label =>
+            `<div class="badge badge-dash badge-primary"><i class="fa-solid fa-circle-info"></i>
+  ${label}</div>`
+        ).join("");
+
         // 3-7: card inner design
         newCardDiv.innerHTML = `
 <div>
                 <div class="flex justify-between mb-2">
                 <span class="text-gray-400 text-xs">#${issue.id}</span>
-                    <span class="bg-orange-100 text-orange-600 px-2 py-1 rounded text-[10px] font-bold uppercase">${issue.priority}</span>
+                    <span class="badge badge-outline badge-secondary uppercase">${issue.priority}</span>
                     
                 </div>
                 <h2 class="font-bold text-gray-800 mb-2 line-clamp-1">${issue.title}</h2>
                 <p class="text-gray-500 text-xs mb-4 line-clamp-2">${issue.description}</p>
-                <div class="badge badge-outline text-[10px] py-3 mb-4">${issue.label}</div>
+
+                <div class="flex flex-wrap gap-1 mb-3">${labelsBox}</div>
             </div>
             
-            <div class="border-t border-gray-100 pt-3 flex flex-col gap-1 text-[10px] text-gray-400">
+            <div class="border-t border-gray-200 pt-3 flex flex-col gap-1 text-[10px] text-gray-400">
                 <span>By <span class="font-semibold text-gray-700">${issue.author}</span></span>
-                <span>${issue.createdAt}</span>
+                <span>${new Date(issue.createdAt).toLocaleDateString()}</span>
             </div>
 `
         // 3-8: append to container
@@ -101,7 +129,6 @@ closedBtn.addEventListener('click', () => loadAllIssues('closed'))
 
 // 06: search function
 searchInput.addEventListener('keyup', (event) => {
-
     // user data collect
     const searchText = event.target.value.toLowerCase();
 
@@ -113,14 +140,14 @@ searchInput.addEventListener('keyup', (event) => {
 
             const allIssues = obj.data;
 
-            // filtering 
-            const search = allIssues.filter(issue => 
+            //6-2: filtering title and description 
+            const search = allIssues.filter(issue =>
                 issue.title.toLowerCase().includes(searchText) ||
                 issue.description.toLowerCase().includes(searchText)
-);
-    displayIssues(search)
+            );
+            displayIssues(search)
 
-})
+        })
 })
 
 // function call
